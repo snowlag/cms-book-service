49dfaf35c4c8da07a3cc8bcd9800446be2dac728
Ankit Joshi <ankitjoshicr7@gmail.com>
2020-11-24 23:03:51 +0530

    Fixed production bugs

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(uploadFiles);
 // app.use(express.static('client/build'));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
+    app.use(express.static('Client/build'));
     app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
 });
